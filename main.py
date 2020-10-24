6fae27ef158d62359f8105b0bda7ec6cec374e3a
Akylbek005 <iakylbek005@gmail.com>
2020-10-24 12:39:48 +0600

    1

---
+++ main.py
@@ -0,0 +1,98 @@
+#1 задание
+#number = int(input("Введите первое число:"))
+#number2 = int(input("Введите второе число:"))
+#print(number - number2)
+
+#2 задание
+#print(36 % 5)
+
+#3 задание# a = len(number)
+
+# a = input("Вве/home/akylbek/PycharmProjects/pythonProject/main.pyдите все что хотите:" )
+# b = (a [::-1])
+# if a == b:
+#     print('Это полиндром')
+# else:
+#     print('Это не полиндром')
+
+#4 задание
+# a = "I love java"
+# b = a.replace("java", "Python")
+# print(b)
+
+#5 задание
+# name = input("Введите свое имя:")
+# print(name * 10)
+
+#6 задание
+# a = input("Введите все что хотите:" )
+# b = (a [::-1])
+# print(b)
+
+#7 задание ***
+# number = input("введите 10 цифр:")
+# a = len(number)
+# if 9 < a:
+#      print(number[9])
+# else:
+#     print("Напишите больше цифр")
+
+
+#8 заданиe
+# a = str(input("Введите все что хотие:"))
+# b = len(a)
+# c = a[::-1]
+# if b > 4:
+#     print(a[0:2]+c[1]+c[0])
+# elif b == 2:
+#     print(a)
+# elif b < 2:
+#     print("Error Error")
+# else:
+#     print(a * 2)
+
+#9 задание
+# a = int(input("Введите любое число:"))
+# print(1+a)/home/akylbek/PycharmProjects/pythonProject/main.py
+# print(a-1)
+
+#10 задание
+# a = input("Введите первое число:")
+# b = input("Введите второе число:")
+# if a < b:
+#     print(a, "<", b)
+# elif a > b:laptos.update(laptos2)
+#     print(a, "/home/akylbek/PycharmProjects/pythonProject/main.py>" ,b)
+# else:
+#     print(a ,"равно",b)
+
+#11 задание
+#a = int(input("Введите любое число"))
+#if a > 0:
+#    print("Ваше число положительное")
+#elif a < 0:
+#    print("Ваше число отрицательное")
+#else:
+#    print('Ваше число не является ни положительным, ни отрицательным')
+
+
+
+# number = input("введите 10 цифр:")
+# a = len(number)
+# if 9 < a:# number = input("введите 10 цифр:")
+# a = len(number/home/akylbek/PycharmProjects/pythonProject/main.py)# a = len(number)
+
+# if 9 < a:
+#     print(number[9])
+# else:
+#     print("Напишите больше цифр")
+
+#     print(number[9])
+# else:
+#     print("Напишите больше цифр")
+# number = input("введите 10 цифр:")
+# a = len(number)
+# if 9 < a:
+#     print(number[9])
+# else:
+#     print("Напишите больше цифр")
---
+++ my.py
@@ -0,0 +1,164 @@
+# list = ['apple','banana','pineapple','strawbery','apple']
+# print(list.count('apple'))
+# string = 'hello wirld'
+# print(string.count('l'))
+# new_list = list(list)
+# print(new_list)
+
+# list = [1,2,3,4,5,6,7,8]
+# list.reverse()
+# print( list )
+
+# string = 'My name is Akylbek'
+# list_ = list(string)
+# list_.reverse()
+# new_list = ''.join(list_)
+# print(new_list)
+
+#метод sort сортирует по ключу
+
+# number = [1, 3, 7 ,2 ,9, 5, 4,]
+# number.sort(reverse = True)
+# print(number)
+
+# a = ['apple','banana','pineapple','strawbery','apple']
+# a.sort(reverse=True)
+# print(a)
+
+# list_ = ['apple','banana','pineapple','strawbery','apple']
+# new_list = list_.copy()
+# print(list_)
+# print(new_list)
+#
+# num1 = [1,2,3]
+# num2 =[4,5,6]
+# num1.extend(num1)
+# num1 += num2
+# print(num1)
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8,]
+# # print(nums)
+# #nums.clear()
+# print(nums[0:4:2])
+
+# tuple = (1,2)
+# a = type(tuple)
+# print(a)
+
+# number = (1, 2, 3, 4, 5, 6, 7, 8, 9,)
+# print(number.count(4))
+# print(number.index(3))
+
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+# zero = number.pop(0)
+# print(number)
+
+# cars = ['mers','bmv','tayota', 'lamborgini']
+# car = cars.pop()
+# print(cars)
+
+#                                            Словарь
+
+#dict_with_pairs = {'ключ':"Значение"}
+# dict_ = {3:1,2:34,5:2 }
+# print(dict_)
+
+# names = {'name1': "john",'name2':"Akylbek"}
+# print(names['name2'])
+
+# dictionary = dict.fromkeys(["key1", 'key2'])
+# dictionary2 = dict.fromkeys(['key2','key1'],['val','val2'])
+# print(dictionary2)
+
+# test = {'name':'show','hello':'world'}
+# print(test['name'])
+# print(test['hello'])
+#
+# some_test = {1:4,2:8,3:12}
+# some_test[4] = 10+20
+# print(some_test)
+# some_test['key'] = 'hi'
+# print(some_test)
+# some_test ['key2'] = [1,2,3,4]
+# some_test['key3'] = {1:2, 2:2}
+# print(some_test)
+# some_test['key3']['key'] = {3:3, 4:4}
+# print(some_test)
+# some_test['key3'] = 'значение'
+# print(some_test)
+
+
+# cars = {'BMW': 'x5','toyota':'camry'}
+# print(cars.get('BMW'))
+# print(cars['BMW'])
+
+# cars = {'BMW': 'x5','toyota':'camry'}
+# print(cars.keys())
+# cars_list = cars.keys()
+# print(cars_list)
+# print(len(cars))
+# mersedes = cars.get('BMW')
+# print(mersedes)
+
+# cars = {'BMW': 'x5','toyota':'camry'}
+# print(cars.values())
+# print(type(cars.values()))
+
+# dict_ = {'first':'1st','second':'2nd'}
+# print(dict_)
+# first = dict_.pop('first')
+# print(dict_)
+# print(first)
+
+# cars = {'BMW': 'x5','toyota':'camry'}
+# deleted = cars.popitem()
+# print(deleted)
+# laptos = {'Lenova':'yoda','macbook':'pro','asus':'cool'}
+# laptos2= {'hello':'world'}
+# laptos3= {'hell2':'world'}
+# print(laptos)
+#
+# laptos.update(laptos2)
+# laptos.update(laptos3)
+# print(laptos)
+
+# dict_ = {'key':'1','key2':'2','key3':'3'}
+# new_dict = dict_.setdefault('key2')
+# print(new_dict)
+# new = dict_['key2']
+# print(new)
+# a = dict_.get('key2')
+# print(a)
+
+# capital = dict(Russia = 'Moscow'Kyrgisnan ="bishkek", USA = 'Washington')
+# print(capital)
+
+# capital = ([('Russia','Moscow'),("Usa","NewYork")])
+# print(capital)
+
+# set_ = [1,2,3,5,2,8,1,0,9]
+# print(set_)
+# set_ = set(set_)
+# print(set_)
+
+# set_ = {1,2,5}
+# print(set_)
+# set_.add(4)
+# print(set_)
+
+
+# set_ = {1,2,5,5,9,5,3,1,4,6,5,4,4,7,9,0,2}
+# print(set_)
+# set_.remove(1,)
+# print(set_)
+
+# set_.discard(0)
+# print(set_)
+#
+# set_ = {2,5,5,9,5,3,1,4,6,5,4,4,7,9,0,2}
+# print(set_)
+# print(set_.pop())
+# print(set_)
+#
+
+
